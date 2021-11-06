66c5157a11d8bbd58dabc2b9fee80bd6079c96c9
Yulia Novikova <killmepalas@gmail.com>
2021-11-06 20:36:23 +0300

    Initial commit

---
+++ Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS_distance_learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
