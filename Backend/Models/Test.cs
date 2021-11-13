0996226e127843cb5c26e115783ffdcd48076a33
Yulia Novikova <killmepalas@gmail.com>
2021-11-13 22:47:00 +0300

    Added course, role and test models

---
+++ Backend/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS_distance_learning.Models
+{
+    public class Test
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
