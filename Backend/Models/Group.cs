0b80a3d5dc582f8ff6a810e312894967b9f40107
Yulia Novikova <killmepalas@gmail.com>
2021-11-14 20:11:44 +0300

    Added group model and group controller

---
+++ Backend/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IS_distance_learning.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        
+        [Required]
+        [MaxLength(10)]
+        public string Code { get; set; }
+    }
+}
