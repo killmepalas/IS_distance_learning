f06d12185e3a65367f320ce689cde8e5902bdf6d
Yulia Novikova <killmepalas@gmail.com>
2021-11-16 14:30:57 +0300

    Added answer and question models, edited test model

---
+++ Backend/Models/Answer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IS_distance_learning.Models
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Text { get; set; }
+        
+        [Required]
+        public bool IsRight { get; set; }
+        
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
---
+++ Backend/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace IS_distance_learning.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Text { get; set; }
+        
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+    }
+}
