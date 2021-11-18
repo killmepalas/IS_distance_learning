69950935586b6927e80135dc1bede7edbd6fa856
olkamrr <olkamrr@ya.com>
2021-11-18 13:03:31 +0300

    small refactors

---
+++ src/Models/StudentAttempt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS_distance_learning.Models
+{
+    public class StudentAttempt
+    {
+        [Key]
+        public int Id { get; set; }
+                
+        [Required]
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,6 @@ namespace IS_distance_learning
             CreateHostBuilder(args).Build().Run();
         }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
